b8f9b56e6a4756ff4a6ac56c7986f1a0b1dca801
Sara <asara9086@gmail.com>
2025-08-12 20:31:50 +0300

    add register, working on login

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { Toaster } from "react-hot-toast";
 import './index.css'
 import App from './App.jsx'
 import { store } from './store/store.js'
@@ -9,6 +10,7 @@ import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster />
       <App />
     </Provider>
   </StrictMode>,
