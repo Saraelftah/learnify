202fe6e8960fa896e527e91be04d14cc879edad8
aya <aya.gamal71994@gmail.com>
2025-08-12 03:20:42 +0200

    recommended teachers carousel + edit stars component + add animation to banner home page

---
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -0,0 +1,113 @@
+import { faHeart, faLightbulb, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import bannerImg from "../../assets/images/banner-home.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+
+function HomeBanner() {
+    const teachers = useSelector((state) => state.teachers.teachers);
+
+    const subjects = teachers.map((t) => t.subject);
+    const [text, setText] = useState("");
+    const [wordIndex, setWordIndex] = useState(0); 
+    const [charIndex, setCharIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    useEffect(() => {
+        if (subjects.length === 0) return;
+
+        const currentWord = subjects[wordIndex];
+        const typingSpeed =  100;
+
+        const timer = setTimeout(() => {
+            if (!isDeleting) {
+                if (charIndex < currentWord.length) {
+                    setText(currentWord.substring(0, charIndex + 1));
+                    setCharIndex(prev => prev + 1);
+                } else {
+                    setTimeout(() => setIsDeleting(true), 1500);
+                }
+            } else {
+                if (charIndex > 0) {
+                    setText(currentWord.substring(0, charIndex - 1));
+                    setCharIndex(prev => prev - 1);
+                } else {
+                    setTimeout(() => {
+                        setIsDeleting(false);
+                        setWordIndex(prev => (prev + 1) % subjects.length);
+                    }, 500);
+                }
+            }
+        }, typingSpeed);
+
+        return () => clearTimeout(timer);
+    }, [charIndex, isDeleting, wordIndex, subjects]);
+
+    
+  return (
+    <>
+        {/* Banner section */}
+        <section className="home__banner text-left bg-[#eaf9f9] p-[50px]">
+          <div className="container">
+            <div className="flex flex-col items-center lg:flex-row gap-4 ">
+              <div className="w-full lg:w-1/2 p-4">
+                <h1 className="text-[var(--dark-color)] !text-[50px] mb-5">
+                    find the most exciting 
+                    <h4 className="h-[50px] font-bold text-[var(--secondary-color)]">{text}</h4>
+                    courses
+                </h1>
+                <p className="text-[var(--text-color)] leading-[var(--line-height)]">
+                  Learn smarter, anywhere, with expert-led, hands-on lessons.
+                  Master new skills with confidence and unlock opportunities for
+                  your future.
+                </p>
+                <div className="banner-search mt-5">search component</div>
+              </div>
+              <div className=" w-[400px] h-[400px] lg:w-1/2  ">
+                <div className="banner-img  w-[400px] h-[400px] rounded-full relative">
+                  <img
+                    className="w-100 object-cover"
+                    src={bannerImg}
+                    alt="banner"
+                  />
+
+                  <div className="chat chat-start absolute top-10 right-[-130px] opacity-90">
+                    <div className="chat-bubble bg-[var(--light-secondary-color)] w-fit max-w-full whitespace-nowrap px-3 py-2">
+                      <FontAwesomeIcon
+                        className="text-[#d53232] mr-2"
+                        icon={faHeart}
+                      />
+                      Great Teacher
+                    </div>
+                  </div>
+
+                  <div className="chat chat-start absolute top-40 right-[-130px] opacity-90">
+                    <div className="chat-bubble bg-[var(--light-secondary-color)] w-fit max-w-full whitespace-nowrap px-3 py-2">
+                      <FontAwesomeIcon
+                        className="text-[var(--primary-color)] mr-2"
+                        icon={faThumbsUp}
+                      />
+                      Super Clear
+                    </div>
+                  </div>
+
+                  <div className="chat chat-start absolute bottom-20 right-[-130px] opacity-90">
+                    <div className="chat-bubble bg-[var(--light-secondary-color)] w-fit max-w-full whitespace-nowrap px-3 py-2">
+                      <FontAwesomeIcon
+                        className="text-[var(--secondary-color)] mr-2"
+                        icon={faLightbulb}
+                      />
+                      Amazing Session
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default HomeBanner
---
+++ src/components/RecommendedTeachers/RecommendedTeachers.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { useSelector } from "react-redux";
+import "./RecommendedTeachers.module.css";
+import RatingStars from "../RatingStars/RatingStars";
+import { NavLink } from "react-router-dom";
+
+function RecommendedTeachers() {
+  const teachers = useSelector((state) => state.teachers.teachers);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <>
+      <div className="recommended-teachers py-[50px]">
+        <h3 className="text-[var(--dark-color)] font-bold mb-5 text-[length:var(--title-font-size)]">recommended teachers</h3>
+        <Carousel
+          responsive={responsive}
+          itemClass="px-2"
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={2000}
+          customTransition="all .5"
+          transitionDuration={500}
+        >
+          {teachers
+            .filter((teacher) => teacher.rating > 4)
+            .map((teacher) => (
+              <div
+                key={teacher.id}
+                className="teacher-card p-4 rounded-[var(--border-radius)] border-[var(--light-primary-color)] border-1 flex flex-col items-center"
+              >
+                <img
+                  className="rounded-full !w-50 mb-5"
+                  src={teacher.Image}
+                  alt={teacher.name}
+                />
+                <h3 className="text-[var(--primary-color)]  font-bold">
+                  {teacher.name}
+                </h3>
+                <p>{teacher.subject}</p>
+                <RatingStars value={teacher.rating} />
+                <NavLink
+                  to="/"
+                  className="btn border-[var(--secondary-color)] bg-white text-[var(--secondary-color)] w-fit"
+                >
+                  view more
+                </NavLink>
+              </div>
+            ))}
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
+export default RecommendedTeachers;
